4bdbfe9abee65fb899fb1e1607fed5ba356d44cd
michelle-lo <mlo20@stuy.edu>
2020-10-28 16:36:47 -0400

    Added BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,11 @@
+public class BankAccount {
+  //instance variables
+  private double balance;
+  private int accountID;
+  private String password;
+   
+  public static void main (String[] args) {
+
+  }
+  //add methods here...
+}
