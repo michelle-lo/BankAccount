1182921211ae039ed51d0e330330969839679f93
michelle-lo <mlo20@stuy.edu>
2020-10-28 17:02:35 -0400

    get methods for acc ID + balanec

--- BankAccount.java
+++ BankAccount.java
@@ -3,7 +3,22 @@ public class BankAccount {
   private double balance;
   private int accountID;
   private String password;
-   
+
+  //constructor -- intialize stuff
+  public BankAccount (int acc, String pass) {
+    balance = 0;
+    accountID = acc;
+    password = pass;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public int getAccountID() {
+    return accountID;
+  }
+
   public static void main (String[] args) {
 
   }
---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+  public static void main (String[] args) {
+    BankAccount b1 = new BankAccount(9, "hello");
+    System.out.println(b1.getBalance()); //0.0
+    System.out.println(b1.getAccountID()); //9
+
+    
+  }
+  //add methods here...
+}
